2e8944075830f79135bb42eb74d5c0f02763bbaf
Lino Oropeza <linooropeza@gmail.com>
2021-10-25 13:07:27 -0400

    Basic window

---
+++ asteroids.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'ruby2d'
+require 'vector2d'
+
+# Setting up the window
+set({ title: 'Asteroids',
+      width: 1024,
+      height: 768,
+      background: 'black' })
+
+show
