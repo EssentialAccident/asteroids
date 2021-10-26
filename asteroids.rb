c6f21d40355d551bedd631091a72465cb8c2021b
Lino Oropeza <linooropeza@gmail.com>
2021-10-26 13:12:21 -0400

    Added the asteroids.rb dependency to the main window

--- asteroids.rb
+++ asteroids.rb
@@ -1,12 +1,22 @@
 # frozen_string_literal: true
 
+# Required Gems
 require 'ruby2d'
 require 'vector2d'
+
+# Require internal files
+require_relative 'lib/asteroid'
 
 # Setting up the window
 set({ title: 'Asteroids',
       width: 1024,
       height: 768,
       background: 'black' })
+
+asteroids = []
+15.times do
+  position = Vector2D.new(rand(Window.width), rand(Window.height))
+  asteroids.push(Asteroid.new(position))
+end
 
 show
