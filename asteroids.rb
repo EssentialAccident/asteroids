0c01bb5ade4ffc428873c9203ece2f94ae058c93
Lino Oropeza <linooropeza@gmail.com>
2021-10-26 20:48:28 -0400

    Added movement to the asteroids

--- asteroids.rb
+++ asteroids.rb
@@ -8,16 +8,26 @@ require 'vector2d'
 require_relative 'lib/asteroid'
 require_relative 'lib/line_vector'
 
+# Constants for the game
+MAX_VELOCITY = 1.0
+
 # Setting up the window
 set({ title: 'Asteroids',
       width: 1024,
       height: 768,
       background: 'black' })
 
+# Setting up the game
+
+# Setting up asteroids
 asteroids = []
 15.times do
   position = Vector2D.new(rand(Window.width), rand(Window.height))
   asteroids.push(Asteroid.new(position))
+end
+
+update do
+  asteroids.map(&:update)
 end
 
 show
