49f277b687cf141d9ed2d9c83bf17fb9cb05ff12
EssentialAccident <linooropeza@gmail.com>
2021-10-27 20:31:15 -0400

    Fix up some mistakes in sintax

--- lib/triangle_vector.rb
+++ lib/triangle_vector.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 class TriangleVector < Ruby2D::Triangle
-  def initialize(opts={})
+  def initialize(opts = {})
     if opts.key?(:point1)
-      opts[x1:] = opts(:point1).x
-      opts[y1:] = opts(:point1).y
+      opts[:x1] = opts[:point1].x
+      opts[:y1] = opts[:point1].y
     end
     if opts.key?(:point2)
-      opts[x2:] = opts(:point2).x
-      opts[y2:] = opts(:point2).y
+      opts[:x2] = opts[:point2].x
+      opts[:y2] = opts[:point2].y
     end
     if opts.key?(:point3)
-      opts[x3:] = opts(:point3).x
-      opts[y3:] = opts(:point3).y
+      opts[:x3] = opts[:point3].x
+      opts[:y3] = opts[:point3].y
     end
     super opts
   end
